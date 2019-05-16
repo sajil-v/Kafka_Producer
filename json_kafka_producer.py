ad1c5099d1dbad2205cce8c8413ca3fadb30c2f9
sajil-v <50611242+sajil-v@users.noreply.github.com>
2019-05-16 20:22:47 +0530

    Update json_kafka_producer.py

--- json_kafka_producer.py
+++ json_kafka_producer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from kafka import KafkaProducer
+from json import dumps
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x: dumps(x).encode('utf-8'))
