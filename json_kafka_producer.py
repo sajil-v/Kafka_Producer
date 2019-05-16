d0aac478d3c6893c51d2cb85beb80a4bf5077f2a
sajil-v <50611242+sajil-v@users.noreply.github.com>
2019-05-16 20:23:50 +0530

    Update json_kafka_producer.py

--- json_kafka_producer.py
+++ json_kafka_producer.py
@@ -7,7 +7,7 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x: dumps(x).encode('utf-8'))
 # "message-topic" is the topic name
 # data is the message send
-data = {'number' : e}
+data = {'msgs' : hello everyone}
 producer.send('message-topic', value=data)
 producer.flush()
 
